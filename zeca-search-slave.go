10172bca60b14986b8c25556dabb5f655febd95d
Jose Geraldo de Carvalho Pereira <zgcarvalho@gmail.com>
2017-04-07 14:39:31 -0300

    changes to simplify config

--- zeca-search-slave.go
+++ zeca-search-slave.go
@@ -3,48 +3,45 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
 	_ "net/http/pprof"
-	"os"
 	"runtime"
 
-	"github.com/BurntSushi/toml"
 	"github.com/jgcarvalho/zeca-search/search"
 )
 
-func run(fnconfig string) {
-	var conf search.Config
-	md, err := toml.DecodeFile(fnconfig, &conf)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if len(md.Undecoded()) > 0 {
-		fmt.Printf("Chaves desconhecidas no arquivo de configuração: %q\n", md.Undecoded())
-		fmt.Printf("Chaves conhecidas: %q\n", md.Keys())
-		fmt.Println("Configuration:", conf)
-		return
-	}
-	fmt.Println("Configuration:", conf)
-	search.RunClient(conf)
-}
+// func run(fnconfig string) {
+// 	var conf search.Config
+// 	md, err := toml.DecodeFile(fnconfig, &conf)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	if len(md.Undecoded()) > 0 {
+// 		fmt.Printf("Chaves desconhecidas no arquivo de configuração: %q\n", md.Undecoded())
+// 		fmt.Printf("Chaves conhecidas: %q\n", md.Keys())
+// 		fmt.Println("Configuration:", conf)
+// 		return
+// 	}
+// 	fmt.Println("Configuration:", conf)
+// 	search.RunClient(conf)
+// }
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	// flags
-	fnconfig := flag.String("config", "default", "Configuration file")
-	profile := flag.Bool("profile", false, "profile")
+	profile := flag.Bool("profile", true, "profile")
 	flag.Parse()
 
+	// Profile
 	if *profile {
 		go http.ListenAndServe(":8081", http.DefaultServeMux)
 	}
 
-	if *fnconfig == "default" {
-		run(os.Getenv("GOPATH") + "/src/github.com/jgcarvalho/zeca-search-master/config.toml")
-	} else {
-		run(*fnconfig)
+	serverIP := flag.Arg(0)
+	if serverIP == "" {
+		fmt.Println("Please, tell me the zeca-server IP.")
+		return
 	}
-
+	search.RunClient(serverIP)
 }
